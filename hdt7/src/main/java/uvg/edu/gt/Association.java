ad98f86dffa53bdfea8dcf7795601e2374087f30
Fercho1118 <rue23748@uvg.edu.gt>
2024-03-21 11:54:51 -0600

    Creación de la clase Association
    Se creó la clase Association para representar las asociaciones entre palabras en inglés y sus equivalentes en español.

---
+++ hdt7/src/main/java/uvg/edu/gt/Association.java
@@ -0,0 +1,24 @@
+package uvg.edu.gt;
+
+public class Association<K, V> {
+    private K key;
+    private V value;
+
+    public Association(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + key + " " + value + ")";
+    }
+}
